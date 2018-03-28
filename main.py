8a7702107300fe3ab310dae32fa38fabeedcbc33
François Boulogne <devel@sciunto.org>
2018-03-28 15:20:37 +0200

    code clean up

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ Created on Thu Mar 22 14:15:31 2018
 """
 
 
-from IPython import get_ipython
-get_ipython().magic('reset -sf')
+#from IPython import get_ipython
+#get_ipython().magic('reset -sf')
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,14 +35,9 @@ from pendant_drop_functions import find_circle,\
 
 def young_laplace(variables, image_shape):#,radius,R_python,Z_python):
 
-
-    #    gamma=variables[0]
-#    theta=variables[1][0]
-#    center_yb=variables[1][1]
-
-    gamma=variables[0]
-    theta=variables[1]
-    center_y=variables[2]
+    gamma = variables[0]
+    theta = variables[1]
+    center_y = variables[2]
 
 #    base_center=[(base_line[-1]+base_line[0])/2,image1.shape[0]-1]
 
@@ -55,16 +50,16 @@ def young_laplace(variables, image_shape):#,radius,R_python,Z_python):
 #    center_y=583.5
 #    center_y=guess_tipy
 #    center_y=562
-    tip_x=guess_tipx
+    tip_x = guess_tipx
 
     print(center_y)
 
-    base_center=[center_y,center_x]
+    base_center = [center_y,center_x]
 #    base_center=[center_yb,tip_x]
 
-    calib=0.00124/400#400 pixel = 1.24mm
-    rho_g=9810#1000*9.81
-    lc=(gamma/rho_g)**(1/2)##We give capillary lengthy : may be given by the user later on
+    calib = 0.00124/400  #400 pixel = 1.24mm
+    rho_g = 1000 * 9.81
+    lc = np.sqrt(gamma / rho_g)  # We give capillary lengthy : may be given by the user later on
 
 #    print(radius)
 #    R,Z=theoretical_contour(image,lc,radius,tip)
@@ -98,34 +93,28 @@ def young_laplace(variables, image_shape):#,radius,R_python,Z_python):
 
     python=[R_python,Z_python]
 
-    ksi_z,kk,mini_inds,RMSd=error_calculation(R,Z,python)
-#    ksi_z,kk,mini_inds,RMSd=error_calculation_2(R,Z,R_python,Z_python,center_y)
-#    print(tak)
-#    ksi_z,kk,mini_inds,RMSd=error_calculation_2(R[tak:],Z[tak:],R_python,Z_python,center_y)
-    return ksi_z,R_python,Z_python,R,Z,mini_inds,RMSd
-#    return ksi_z,R_python,Z_python,R,Z,RMSd
+    ksi_z, kk, mini_inds, RMSd = error_calculation(R, Z, python)
+
+    return ksi_z, R_python, Z_python, R, Z, mini_inds, RMSd
 
 
 def error_f(variables, image_shape):
-    print(variables)
+    print("variables:",  variables)
     error,R_python,Z_python,R,Z,mini_inds,RMSd = young_laplace(variables, image_shape)
-#    error,R_python,Z_python,R,Z,RMSd=young_laplace(variables)
     return RMSd
-
-
 
 
 def load_image(path, region=None):
     """
     Load an image.
-    
+
     Parameters
     ----------
     path : string
         File path.
     region : tuple, optional
         Corner positions to crop the image.
-    
+
     """
     image = io.imread(image_path, as_grey=True)
 
@@ -139,32 +128,32 @@ if __name__ == '__main__':
 
     image_path = 'uEye_Image_000827.bmp'
     zoom = ([100,1312], [800,1900])
-    
-    
-    
+
+
+
     #image_path = 'uEye_Image_002767.bmp'
-    
-    
+
+
     image1 = load_image(image_path, region=zoom)
-    
+
     edges = feature.canny(image1, sigma=2.5)
-    
+
     hough_radii = np.arange(418, 440)
-    
+
     center_x, center_y, radius, tip = find_circle(edges, hough_radii)
-    
+
     rr,cc = circle(center_x, center_y, radius-5)
     image1[rr, cc] = 10
 
-    
+
     Z_python=np.where(edges==True)[0]
     R_python=np.where(edges==True)[1]
-    
-    
-    
+
+
+
     ###arbitrary first guess for gamma
     gamma0 = 0.040 # N/m
-    
+
     #####guess gravity angle
 #    c_center = [center_y, center_x]
 #    base_line = np.where(image1[image1.shape[0]-1,:]<5)[0]
@@ -172,16 +161,16 @@ if __name__ == '__main__':
 #    hyp = np.sqrt(abs(base_center[0]-tip[0])**2+abs(base_center[1]-tip[1])**2)
 #    adj = image1.shape[0]-1-tip[1]
 #    theta = -np.arccos(adj/hyp) * 180 / np.pi
-    
+
     c_center = [center_y,center_x]
     base_line = np.where(image1[image1.shape[0]-1,:]<5)[0]
     base_center = [(base_line[-1]+base_line[0])/2,image1.shape[0]-1]
     hyp = np.sqrt((base_center[0]-c_center[0])**2+(base_center[1]-c_center[1])**2)
     adj = image1.shape[0]-1-c_center[1]
     opp = abs(base_center[0]-c_center[0])
-    
+
     theta = np.arccos(adj/hyp) * 180 / np.pi
-    
+
     if center_y>base_center[0] and theta>0:
         guess_tipy=(edges.shape[0]-1-tip[1])*np.tan(abs(theta)*np.pi/180)+base_center[0]
     else:
@@ -189,16 +178,16 @@ if __name__ == '__main__':
     #guess_tipy=tip[0]
     #guess_tipx=Z[np.where(abs(np.array(R)-guess_tipy)==min(abs(np.array(R)-guess_tipy)))[0][0]]
     #guess_cy=base_center[0]
-    
+
     ind_min = np.where(abs(np.array(R_python)-guess_tipy)==min(abs(np.array(R_python)-guess_tipy)))[0][0]
     guess_tipx = Z_python[ind_min]
     #theta=-2.06
-    
+
     tipx=tip[1]
     tipy=tip[0]
     guess_cy=584
-    
-    
+
+
     initial_gammas=np.divide([-.02,.02,-.02,.02],10)#,-.005,.005,-.001,.001],10)
     #initial_radii=np.divide([-15,-15,15,15],10)#,-5,5,-1,1],10)
     initial_thetas=np.divide([-.02,-.02,.02,.02],5)*180/np.pi#,-.005,.005,-.001,.001],10),0)*180/np.pi#[-.001,.001]#[-.001,.001]#
@@ -208,35 +197,35 @@ if __name__ == '__main__':
     #
     initial_directions=np.transpose(np.array([initial_gammas,initial_thetas,initial_center_y]))#,initial_radii,initial_center_yb]))#,initial_center_yb]))#,initial_center_xb]))
     variables=[gamma0,theta,center_y]#,radius,guess_tipy]#,guess_tipx]#,guess_cy]#,guess_tipx]#,center_x]
-    
+
     #rotation_param=[theta,center_y]
     #variables=[gamma0,rotation_param]
     #
     #initial_gammas=np.array([-.002,.002,-.002,.002])
     #initial_rotation_param=np.array([list([-1,1,-1,1]*1/radius),[1,1,-1,-1]])
     #initial_directions=[initial_gammas,initial_rotation_param]
-    
+
     ###http://informatik.unibas.ch/fileadmin/Lectures/HS2013/CS253/PowellAndDP1.pdf slides about minimizations methods
     res = minimize(error_f, variables, args=(edges.shape,), method='Powell',
                    options={'direc':initial_directions,'maxiter':100,'xtol': 1e-3,'ftol':1e-2, 'disp': True})
     #,options={'xtol': 1e-8, 'disp': True,'maxfev':100})
     optimal_variables = res.x
-    
+
     #res = fmin_powell(error_f, variables, direc=initial_directions,maxiter=100)#'xtol': 1e-3,'ftol':1e-2, 'disp': True})#,options={'xtol': 1e-8, 'disp': True,'maxfev':100})
     #optimal_variables=res
-    
-    
+
+
     error, R_python, Z_python, R, Z, mini_inds, RMSd = young_laplace(optimal_variables, edges.shape)
     #error,R_python,Z_python,R,Z,RMSd=young_laplace(optimal_variables)
     image1b = rotate(image1,optimal_variables[1],center=base_center,resize=False)
-    
-    
+
+
     center_yb, center_xb = rotate_lines([center_y], [center_x], tip, optimal_variables[1])
-    
+
     center_yb = center_yb[0]
     center_xb = center_xb[0]
-    
-    
+
+
     plt.figure()
     ax=plt.axes()
     plt.imshow(image1,cmap='gray')
@@ -245,10 +234,10 @@ if __name__ == '__main__':
     plt.plot(R_python,Z_python,'*g',markersize=1)
     plt.plot(R,Z,'*b',markersize=1)
     plt.plot([base_center[0],tip[0]],[base_center[1],tip[1]],'-y')
-    
+
     #R_dist=[]
     #Z_dist=[]
-    
+
     #R_dist_l=[]
     #Z_dist_l=[]
     #R_dist_r=[]
--- pendant_drop_functions.py
+++ pendant_drop_functions.py
@@ -14,8 +14,8 @@ from skimage.transform import hough_circle
 def find_circle(edges, hough_radii):
     """
     Find the best circle to model the drop tip.
-    
-    
+
+
     """
 
 
@@ -46,14 +46,14 @@ def find_circle(edges, hough_radii):
 
 
 def theoretical_contour(image_shape, lc, radius, tip):
-    calib=0.00124/400#400 pixel = 1.24mm
-    r0=radius*calib
-    r_st=r0/lc
+    calib = 0.00124/400#400 pixel = 1.24mm
+    r0 = radius*calib
+    r_st = r0 / lc
 
-    Z0=image_shape[0]-tip[1]#image.shape[0]-50##Position de Z0 w/r x (coordonnée verticale)
-    Zmax=Z0/lc*calib#maximum possible values of Z to be upgraded
-    nPoints=Z0*2
-    ds=Zmax/nPoints
+    Z0 = image_shape[0] - tip[1]  #image.shape[0]-50##Position de Z0 w/r x (coordonnée verticale)
+    Zmax = Z0 / lc * calib #maximum possible values of Z to be upgraded
+    nPoints = Z0 * 2
+    ds = Zmax / nPoints
     #####Bouadary for validity of sperical approx
     if r_st <1:
         sLimit=r_st*0.2
@@ -145,7 +145,7 @@ def rotate_lines(R,Z, center, theta):
     return R_rot,Z_rot
 
 
-def partial_ksi(theorique,python):
+def partial_ksi(theorique, python):
     ####calculate the location on the python contour that minimizes the distance to the theoretical one
     dist=[]
 #    m=0
@@ -162,92 +162,31 @@ def partial_ksi(theorique,python):
         for i in z_inds:
             dist.append(((python[0][i]-theorique[0])**2+(python[1][i]-theorique[1])**2)**(1/2))
     ind=np.where(dist==min(dist))[0][0]
-    return [python[1][z_inds[ind]],python[0][z_inds[ind]]],min(dist)
+    return [python[1][z_inds[ind]], python[0][z_inds[ind]]], min(dist)
 
 
-def error_calculation(R,Z,python):
-    ind_milieu=int(len(Z)/2)
+def error_calculation(R, Z, python):
+    ind_milieu = int(len(Z)/2)
 
-    ksi_z=[]
-    mini_inds=[]
+    ksi_z = []
+    mini_inds = []
     for i in range (ind_milieu):
-        ind_droite=ind_milieu+i
-        ind_gauche=ind_milieu-i
+        ind_droite = ind_milieu+i
+        ind_gauche = ind_milieu-i
         if i==0:
-            min_dist_ind,ksi0=partial_ksi([R[ind_milieu],Z[ind_milieu]],python)
+            min_dist_ind, ksi0 = partial_ksi([R[ind_milieu], Z[ind_milieu]], python)
             mini_inds.append(min_dist_ind)
             ksi_z.append(ksi0)
         else:
-            min_dist_ind,ksi_gauche=partial_ksi([R[ind_gauche],Z[ind_gauche]],python)
+            min_dist_ind,ksi_gauche = partial_ksi([R[ind_gauche], Z[ind_gauche]], python)
             mini_inds.append(min_dist_ind)
-            min_dist_ind,ksi_droite=partial_ksi([R[ind_droite],Z[ind_droite]],python)
+            min_dist_ind,ksi_droite = partial_ksi([R[ind_droite], Z[ind_droite]], python)
             mini_inds.append(min_dist_ind)
             ksi_z.append(ksi_gauche+ksi_droite)
-        n=i*2+1
-        RMSd=np.sqrt(np.sum(ksi_z)/n)
-    return np.sum(ksi_z),ksi_z,mini_inds,RMSd
+        n = i*2+1
+        RMSd = np.sqrt(np.sum(ksi_z) / n)
+    return np.sum(ksi_z), ksi_z, mini_inds, RMSd
 
 
 def error_calculation_2(R,Z,R_python,Z_python,guess_tipy):
-    from scipy.interpolate import interp1d
-
-    R_python_l=R_python[R_python<guess_tipy]
-    Z_python_l=Z_python[R_python<guess_tipy]
-    py_interp_l=interp1d(Z_python_l,R_python_l)
-
-    R_python_r=R_python[R_python>guess_tipy]
-    Z_python_r=Z_python[R_python>guess_tipy]
-    py_interp_r=interp1d(Z_python_r,R_python_r)
-
-    ind_milieu=int(len(Z)/2)
-
-    mini=[[],[]]
-
-    ksi_z=[]
-    for i in range (ind_milieu):
-        if i==0:
-            continue
-        ind_droite=ind_milieu+i
-        ind_gauche=ind_milieu-i
-
-        ksi_gauche=(R[ind_gauche]-py_interp_l(Z[ind_gauche]))**2
-        mini[0].append([float(py_interp_l(Z[ind_gauche])),Z[ind_gauche]])
-
-        ksi_droite=(R[ind_droite]-py_interp_r(Z[ind_droite]))**2
-        mini[1].append([float(py_interp_r(Z[ind_droite])),Z[ind_droite]])
-
-        ksi_z.append(ksi_gauche+ksi_droite)
-    n=i*2
-    RMSd=np.sqrt(np.sum(ksi_z)/n)
-
-#    mini_inds=[]
-#    for i in range (ind_milieu):
-#        if i==0:
-#            continue
-#        ind_droite=ind_milieu+i
-#        ind_gauche=ind_milieu-i
-#
-#        if Z[ind_gauche]>min(Z_python_l):
-#            ksi_gauche=(R[ind_gauche]-py_interp_l(Z[ind_gauche]))**2
-#            mini[0].append([float(py_interp_l(Z[ind_gauche])),Z[ind_gauche]])
-#        else:
-#            ksi_gauche=0
-#        if Z[ind_droite]>min(Z_python_r):
-#            ksi_droite=(R[ind_droite]-py_interp_r(Z[ind_droite]))**2
-#            mini[1].append([float(py_interp_r(Z[ind_droite])),Z[ind_droite]])
-#        else:
-#            ksi_droite=0
-#
-#        ksi_z.append(ksi_gauche+ksi_droite)
-#        if ksi_gauche+ksi_droite>200 and Z[ind_droite]>min(Z_python_r) and Z[ind_gauche]>min(Z_python_l):
-#            print('ksi_gauche='+str(ksi_gauche)+' ; ind_gauche='+str(ind_gauche) + " ; Rinterp="+str(float(py_interp_l(Z[ind_gauche]))))
-#            print('ksi_droite='+str(ksi_droite)+' ; ind_droite='+str(ind_droite) + " ; Rinterp="+str(float(py_interp_r(Z[ind_droite]))))
-#    n=i*2
-#    RMSd=np.sqrt(np.sum(ksi_z)/n)
-
-#    return ksi_z,RMSd
-    return np.sum(ksi_z),ksi_z,mini,RMSd
-#def sort_exp(R_python,Z_python,shape):
-#    for val in range(shape):
-#        level=shape-val-1
-#        np.where(Z_python==level)
+    pass
