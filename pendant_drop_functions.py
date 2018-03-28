1382fec69394cc42aa8ac3c6ffd0a7152c66f4f9
François Boulogne <devel@sciunto.org>
2018-03-28 16:51:03 +0200

    start to simplify contour

--- pendant_drop_functions.py
+++ pendant_drop_functions.py
@@ -45,7 +45,7 @@ def find_circle(edges, hough_radii):
     return center_x, center_y, radius,tip
 
 
-def theoretical_contour(image_shape, lc, radius, tip):
+def old_theoretical_contour(image_shape, lc, radius, tip):
     calib = 0.00124/400#400 pixel = 1.24mm
     r0 = radius*calib
     r_st = r0 / lc
@@ -67,11 +67,7 @@ def theoretical_contour(image_shape, lc, radius, tip):
         R.append(r_st*np.sin(s/r_st)+s**5/(40*r_st**2))
         Z.append(1/(2*r_st)*s**(2)*(1-(0.75+1/(12*r_st**2))*s**2))
         s+=ds
-#    s-=ds
-    #    psi=s/r_st*(1-s**2/8)
 
-#    width=image.shape[1]#(ax.get_ylim()[0]- ax.get_ylim()[1])
-#    height=image.shape[0]
 
     ####Fonction dérivation des paramètres:
     def deriv(variables):
@@ -114,6 +110,77 @@ def theoretical_contour(image_shape, lc, radius, tip):
     R.extend(variables[0][:])
     Z.extend(variables[3][:])
     return R,Z,tak
+
+
+
+def theoretical_contour(image_shape, lc, radius, tip):
+    calib = 0.00124/400#400 pixel = 1.24mm
+    r0 = radius*calib
+    r_st = r0 / lc
+
+    Z0 = image_shape[0] - tip[1]  #image.shape[0]-50##Position de Z0 w/r x (coordonnée verticale)
+    Zmax = Z0 / lc * calib #maximum possible values of Z to be upgraded
+    nPoints = Z0 * 2
+    ds = Zmax / nPoints
+    #####Bouadary for validity of sperical approx
+    if r_st <1:
+        sLimit=r_st*0.2
+    else:
+        sLimit=.2
+
+
+    s = np.arange(0, sLimit, ds)
+    R = r_st*np.sin(s/r_st)+s**5/(40*r_st**2)
+    Z = 1/(2*r_st)*s**(2)*(1-(0.75+1/(12*r_st**2))*s**2)
+
+    # For the following...
+    R = R.tolist()
+    Z = Z.tolist()
+    s = s[-1]
+
+    ####Fonction dérivation des paramètres:
+    def deriv(variables):
+        k=[]
+        k.append(math.cos(variables[1][-1]))#dérivée
+        k.append(variables[2][-1])
+        k.append(-np.sin(variables[1][-1])+np.cos(variables[1][-1])*(np.sin(variables[1][-1])/variables[0][-1]-variables[2][-1])/variables[0][-1])
+        k.append(np.sin(variables[1][-1]))
+        return k
+    #
+    ###initial conditions with approximate solution:
+    nVar=4
+    variables=[[0] for i in range(nVar)]
+    tmp=[[] for i in range(nVar)]
+
+    variables[0][0]=r_st*np.sin(s/r_st)+s**5/(40*r_st**2)
+    variables[1][0]=s*(1-0.125*s**2)/r_st
+    variables[2][0]=(1-0.375*s**2)/r_st
+    variables[3][0]=1/(2*r_st)*s**(2)*(1-(0.75+1/(12*r_st**2))*s**2)
+    Zlocal=0
+    while Zlocal<image_shape[0]-1:
+#    while variables[3][-1]<Zmax:
+        k1=deriv(variables)
+        for i in range(nVar):
+            tmp[i]=[variables[i][-1]+0.5*ds*k1[i]]
+        k2=deriv(tmp)
+        for i in range(nVar):
+            tmp[i]=[variables[i][-1]+0.5*ds*k2[i]]
+        k3=deriv(tmp)
+        for i in range(nVar):
+            tmp[i]=[variables[i][-1]+0.5*ds*k3[i]]
+        k4=deriv(tmp)
+        for i in range(nVar):
+            variables[i].append(variables[i][-1]+ds*(k1[i]+2*(k2[i]+k3[i])+k4[i]))
+        Zlocal=lc/calib*variables[3][-1]+tip[1]#+ax.get_ylim()[1]
+#        Zlocal=lc/calib*variables[3][-1]+tip[1]-10
+
+    tak=len(R)
+
+    R.extend(variables[0][:])
+    Z.extend(variables[3][:])
+    return R,Z,tak
+
+
 
 
 def rotate_lines(R,Z, center, theta):
