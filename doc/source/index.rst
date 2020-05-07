86ead999b27d106ab566ff41e989cd5a7bd0616f
François Boulogne <devel@sciunto.org>
2020-05-07 17:00:52 +0200

    Start documenting API

--- doc/source/index.rst
+++ doc/source/index.rst
@@ -17,6 +17,6 @@ Welcome to PendantDrop's documentation!
 Indices and tables
 ==================
 
+* `API Reference <api/api.html>`_
 * :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
--- drop/__init__.py
+++ drop/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '0.1dev'
+
+#from .io import load_image
+
+from .optimization import (young_laplace,
+                           deviation_edge_model_simple,
+                           deviation_edge_model_full)
