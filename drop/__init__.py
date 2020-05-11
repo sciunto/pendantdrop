4bc2bcb06fbb1417d9a3ba5aa25ff874f69104ee
François Boulogne <devel@sciunto.org>
2020-05-11 11:07:19 +0200

    Enhance lib structure

--- drop/__init__.py
+++ drop/__init__.py
@@ -2,6 +2,6 @@ __version__ = '0.1dev'
 
 #from .io import load_image
 
-from .optimization import (young_laplace,
-                           deviation_edge_model_simple,
-                           deviation_edge_model_full)
+#from .optimize import (young_laplace,
+#                       deviation_edge_model_simple,
+#                       deviation_edge_model_full)
---
+++ drop/improcessing/__init__.py
@@ -0,0 +1,4 @@
+
+from .edge import fit_circle_tip, detect_edges, guess_angle
+
+
