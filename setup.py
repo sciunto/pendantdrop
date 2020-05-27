bfa3e00dcc7a0280e0704ec4ec2ab8714ecf74ab
François Boulogne <fboulogne@sciunto.org>
2020-05-27 09:19:25 +0200

    Add short and long descriptions (#36)

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 
 import setuptools
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     name         = 'pendantdrop',
     version      = '0.1',
@@ -9,7 +16,9 @@ setuptools.setup(
     author       = "Francois Boulogne and other contributors",
     license      = "GPLv3",
     author_email = "devel@sciunto.org",
-    description  = "",
+    description  = "Measure surface tension with pendant drops",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages     = setuptools.find_packages(exclude=['doc', 'benchmarks']),
     install_requires = [],
 )
