d99b373572fabfbbd4432bc495a2e224800dfda3
François Boulogne <devel@sciunto.org>
2020-09-23 08:19:01 +0200

    Switch to dev version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name         = 'pendantdrop',
-    version      = '0.2.0',
+    version      = '0.2.0dev',
     url          = "https://github.com/sciunto-org/pendantdrop",
     author       = "Francois Boulogne and other contributors",
     license      = "GPLv3",
