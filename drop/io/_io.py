b35bc99685f561e01f2e8de755da0df54d02c178
François Boulogne <devel@sciunto.org>
2020-11-06 13:56:38 +0100

    Add additional loading options

--- drop/io/_io.py
+++ drop/io/_io.py
@@ -36,9 +36,11 @@ def _auto_crop(image, expand=20):
     return image[bb[0]-expand:bb[2]+expand, bb[1]-expand:bb[3]+expand]
 
 
-def load_image(path, region=None):
+def load_image(path, region=None, *, flip_updown=False, check_orientation=True):
     """
     Load an image.
+
+    The library assumes that the drop base it at the bottom.
 
     Parameters
     ----------
@@ -46,6 +48,11 @@ def load_image(path, region=None):
         File path.
     region : tuple, optional
         Tuple: corner positions to crop the image.
+    flip_updown : bool, optional
+        If True, flip the image upside down
+    check_orientation : bool, optional
+        Check the orientation of the final image.
+        If the drop base is not at the bottom, an exception is raised.
 
     Returns
     -------
@@ -58,4 +65,15 @@ def load_image(path, region=None):
                       region[1][0]:region[1][1]]
         if len(image.ravel()) == 0:
             raise ValueError('Zero size image. Check that the region is appropriate.')
+
+    if flip_updown:
+        image = image[::-1, :]
+
+    if check_orientation:
+        top = np.max(image[0, :])
+        bottom = np.max(image[-1, :])
+        if top < bottom:
+            raise RuntimeError('The image orientation is detected to be incorrect. '
+                               'The needle must be at the bottom.')
+
     return image
